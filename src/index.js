36ff5a6af684bf88072fa2cb2f2bb9db3b5a1117
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-01-23 19:55:10 +0200

    Add 404 page

--- src/index.js
+++ src/index.js
@@ -22,4 +22,8 @@ app.get('/about', (req, res) => {
 res.render('about'); // Това ще зареди home.hbs без layout
 });
 
+app.get('*', (req, res) => {
+    res.render('404')
+})
+
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
