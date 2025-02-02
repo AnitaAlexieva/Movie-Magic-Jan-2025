132f589c3b4803dfbb5380df7eefacf95a25441f
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-02-03 00:18:08 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import routes from './routes.js'
 import showRatingHelper from './helpers/rating-helper.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
+//db configuration
+
+try{
+    const uri = 'mongodb://localhost:27017/magic-movies-jan2025'
+    await mongoose.console(uri)
+
+    
+}catch(err){
+
+}
+
+//handklebars configuration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers:{
@@ -15,6 +28,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+//express configuration
 //express static да отговаря на route, който да започва със статик 
 //ако нещо отговаря на static да го търси в src/public
 app.use('/static', express.static('src/public'))
@@ -22,6 +36,8 @@ app.use('/static', express.static('src/public'))
 //Learn express to parse from data
 app.use(express.urlencoded({extended:false}))
 
+//setup routes
 app.use(routes)
   
+//start server
 app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
