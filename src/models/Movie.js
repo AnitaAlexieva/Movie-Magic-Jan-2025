5d725a9eb98054446a87e216b41fa8affc521a78
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-02-04 14:07:24 +0200

    Add movie cast many to many realtionship

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model , Types} from "mongoose";
 
 //create Shema
 const movieSchema  = new Schema({
@@ -9,7 +9,13 @@ const movieSchema  = new Schema({
     year: Number,
     imageUrl:String,
     rating:Number,
-    description:String
+    description:String,
+    //всеки филм ще има колекция(масив) от id, 
+    // като всяко id ще представлява връзка(референция) към  оопределение участици
+    casts: [{
+        type: Types.ObjectId,
+        ref:'Cast'
+    }]
 })
 
 //create model
