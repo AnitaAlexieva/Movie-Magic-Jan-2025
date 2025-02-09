6e5071f3a7dbe876d91d77297d3fa7adbca29e7f
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-02-09 21:33:17 +0200

    Add login functionality with jwt generation

--- src/controllers/auth-cotroller.js
+++ src/controllers/auth-cotroller.js
@@ -10,13 +10,24 @@ authController.get('/register', (req, res) =>{
 authController.post('/register', async (req, res) =>{
     const userData = req.body
     
-
-    
     res.redirect('/auth/login')
 })
-
 
 authController.get('/login', (req, res) =>{
     res.render('auth/login')
 })
+
+authController.post('/login', async (req, res) =>{
+    const {email, password} = req.body
+    
+    try{
+        const token = await authService.login(email, password)
+        console.log(token)
+    }catch(err){
+        console.log(err.message)
+        return res.redirect('/404')
+    }
+    res.redirect('/')
+})
+
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,39 @@
 import User from "../models/User.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const SECRET = '92b$10$166F.WCNwiIw389mTT/lmd.zNmejxsBxrI/McZO/RHsX14bsILtJvG'
 
 export default {
     register(userData){
        return User.create(userData)
+    },
+    async login(email, password){
+
+        //Check user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return new Error('Invalid email or password!')
+        }
+
+        //Check password is correct
+        const isValid = await bcrypt.compare(password, user.password)
+        if(!isValid) {
+            throw new Error('Invalid email or password')
+        }
+
+        //Generate token
+        const playload = {
+            id:user.id,
+            email: user.email,
+        }
+
+        //TODO: Use async option
+        const token = jwt.sign(playload, SECRET, {expiresIn: '2h'})
+
+        //return token
+
+
+        return token
     }
 }
