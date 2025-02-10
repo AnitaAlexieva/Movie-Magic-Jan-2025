4dd7a70ae7c62061ab978fe40bff3e281869b44f
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-02-10 11:57:01 +0200

    Show dynamic data in edit form

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -71,8 +71,11 @@ movieController.get('/:movieId/delete',async (req, res) =>{
     res.redirect('/')
 })
 
-movieController.get('/:movieId/edit', (req,res) =>{
-    res.render('movie/edit')
+movieController.get('/:movieId/edit',async (req,res) =>{
+    const movieId = req.params.movieId
+    const movie = await movieService.getOneMovie(movieId)
+
+    res.render('movie/edit', {movie})
 })
 
 export default movieController
